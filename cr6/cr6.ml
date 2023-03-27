5d932da8b21a330b6d110798838d86533ec9d4bb
jaydenpersonnat <89941534+jaydenpersonnat@users.noreply.github.com>
2023-03-26 21:56:54 -0400

    reveal cr6 soln

--- cr6/cr6.ml
+++ cr6/cr6.ml
@@ -5,6 +5,17 @@ let y = !x ;;
 x := 42 ;;
 let z = !x ;; 
 
+(* During the function declaration, we're initializing a reference called memory 
+and having it point to "" *)
+let remember = 
+  let memory = ref "" in 
+  fun msg -> 
+    let previous = !memory in 
+    memory := msg; previous ;; 
+
+
+
+
 (* What is the value of x? *)
 
 let x =
@@ -36,10 +47,14 @@ let m2 : int mlist =
 
 (* Define a function mfirst : int -> ’a mlist -> ’a list that returns a list (im-
 mutable) of the first n elements of a mutable list: *)
-
+let rec mfirst (n : int) (lst : 'a mlist) : 'a list = 
+  if n = 0 then [] else 
+    match !lst with 
+    | Nil -> [] 
+    | Cons (hd, tl) -> hd :: mfirst (pred n) tl ;; 
 
 (* Write code to define a mutable integer list alternating such that for all integers n, the
 expression mfirst n alternating returns a list of alternating 1s and 2s, for example, *)
 
-
-
+let rec alternating =
+  ref (Cons (1, ref (Cons (2, alternating)))) ;;
